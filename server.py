667afbce6b4dd80c05bd84ec98e65617822b1f3b
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-30 22:40:09 -0700

    add detect if guess is too high or too low

--- server.py
+++ server.py
@@ -12,39 +12,47 @@ app.secret_key = "TiYSKDNRitA!"                                                 
 def index():
     if 'num_tries' in session:                                                     # If  num_visits key exists
         session['num_tries'] += 1
-        print("Session key num_tries:", session['num_tries'], ":: my_number:", session['my_number'])
     else:                                                                           # If num_visits key doesn't exist
         session['num_tries'] = 1                                                    #   start a new game
         session['my_number'] = random.randint(1,100)                                #   get a random number from 1 to 100
-        print("Session key num_tries:", session['num_tries'], ":: my_number:", session['my_number'])
+        session['message'] = "start"
+    print("Session key num_tries:", session['num_tries'], ":: my_number:", session['my_number'])
     return render_template("index.html")
 
 @app.route('/game_reset')                                                           # Resets the game
 def game_reset():
     del session['num_tries']
     del session['my_number']
-    del session['number_guess']
-    del session['input_error']
     print("::: Game has been RESET :::")
     return redirect('/')                                                            # return to the root after game is reset
 
 @app.route('/get_guess', methods=['POST'])
 def get_guess():
-    session['number_guess'] = request.form['number_guess']
-    if session['number_guess'].isnumeric():
+    session['number_guess'] = request.form['number_guess']                          # get the input string from the form
+    if session['number_guess'].isnumeric():                                         # check if the input value is a number
         session['number_guess'] = int(session['number_guess'])
-        session['input_error']=False
+        if session['number_guess'] == session['my_number']:                             # Check if player guessed the number correctly
+            session['message'] = f"You got it!<br>The number was {session['my_number']}"
+            session['color'] = "mediumseagreen"
+        elif session['number_guess'] < session['my_number']:
+            session['message'] = "Too Low!"
+            session['color'] = "indianred"
+        else:
+            session['message'] = "Too High!"
+            session['color'] = "indianred"
     else:
         session['number_guess'] = 50
-        session['input_error'] = True
-    
+        session['message'] = "That was a Poor Guess!"
+        session['color'] = 'indianred'
+
+
     # if request.form['number_guess'] != "":
     #     session['number_guess'] = int(request.form['number_guess'])
     #     session['input_error'] = 0
     # else:
     #     session['number_guess'] = 50
     #     session['input_error'] = 1
-    print("Got number guess of:", session['number_guess'], "from form post :: Input Error state:", session['input_error'])
+    print("Got number guess of:", session['number_guess'], "from form post :: Message:", session['message'])
     return redirect("/")
 
 # @app.route('/increment_by', methods=['POST'])
