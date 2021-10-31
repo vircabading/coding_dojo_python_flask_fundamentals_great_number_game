ae26a20ca97854e7728e8fc4dfe9af217a115d39
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-30 23:47:41 -0700

    add win and game over

--- server.py
+++ server.py
@@ -16,6 +16,7 @@ def index():
         session['num_tries'] = 1                                                    #   start a new game
         session['my_number'] = random.randint(1,100)                                #   get a random number from 1 to 100
         session['message'] = "start"
+        session['color'] = 'none'
     print("Session key num_tries:", session['num_tries'], ":: my_number:", session['my_number'])
     return render_template("index.html")
 
@@ -31,17 +32,23 @@ def get_guess():
     session['number_guess'] = request.form['number_guess']                          # get the input string from the form
     if session['number_guess'].isnumeric():                                         # check if the input value is a number
         session['number_guess'] = int(session['number_guess'])
+        session['input_error'] = False
         if session['number_guess'] == session['my_number']:                             # Check if player guessed the number correctly
-            session['message'] = f"You got it!<br>The number was {session['my_number']}"
+            session['message'] = f"You got it!  The number was {session['my_number']}"
             session['color'] = "mediumseagreen"
-        elif session['number_guess'] < session['my_number']:
-            session['message'] = "Too Low!"
-            session['color'] = "indianred"
         else:
-            session['message'] = "Too High!"
-            session['color'] = "indianred"
+            if session['number_guess'] < session['my_number']:
+                session['message'] = "Too Low!"
+                session['color'] = "indianred"
+            else:
+                session['message'] = "Too High!"
+                session['color'] = "indianred"
+            if session['num_tries'] >= 5:
+                session['message']="GAME OVER!"
+                session['color'] = "black"
     else:
         session['number_guess'] = 50
+        session['input_error'] = True
         session['message'] = "That was a Poor Guess!"
         session['color'] = 'indianred'
 
